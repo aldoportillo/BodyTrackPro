55454a60e4a99911fe617d50b608d38c92480842
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-12-16 12:45:26 -0600

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_16_181826) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_16_184339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "fat_percentages", force: :cascade do |t|
     t.integer "percent"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "metrics", force: :cascade do |t|
+    t.integer "weight"
+    t.integer "fat_percentage"
+    t.integer "muscle_percentage"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
