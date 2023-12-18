70be02150087467276adef866e7ceb2945171145
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-12-18 16:25:41 -0600

    Add associations

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -1,2 +1,3 @@
 class Macro < ApplicationRecord
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :metrics, dependent: :destroy
+  has_many :macros, dependent: :destroy
 end
