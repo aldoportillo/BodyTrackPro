300bd6aad79bdaaff067adcd82fec1e55b8fbc4a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-12-18 17:02:54 -0600

    Remove macro removal migrations

--- db/migrate/20231218195533_remove_carb_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCarbFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :carb, :integer
-  end
-end
--- db/migrate/20231218195619_remove_integer_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIntegerFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :protein, :integer
-  end
-end
