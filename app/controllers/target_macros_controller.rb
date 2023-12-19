1bc79bed8294ad3cef91af829e6f6f190e28f0e6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-12-18 18:36:16 -0600

    Clean up controller

--- app/controllers/target_macros_controller.rb
+++ app/controllers/target_macros_controller.rb
@@ -1,17 +1,14 @@
 class TargetMacrosController < ApplicationController
+  before_action :set_or_create_target_macro, only: [:show, :edit, :update]
 
   # GET /target_macros/1 or /target_macros/1.json
   def show
-    @target_macro = current_user.target_macro || current_user.build_target_macro(fat: 0, protein: 0, carb: 0)
+
   end
 
   # GET /target_macros/1/edit
   def edit
-    @target_macro = current_user.target_macro
-    if @target_macro.nil?
-      @target_macro = current_user.build_target_macro(fat: 0, protein: 0, carb: 0)
-      @target_macro.save
-    end
+
   end
 
   # POST /target_macros or /target_macros.json
@@ -31,14 +28,12 @@ class TargetMacrosController < ApplicationController
 
   # PATCH/PUT /target_macros/1 or /target_macros/1.json
   def update
-    @target_macro = current_user.target_macro
     if @target_macro.update(target_macro_params)
       redirect_to target_macro_path, notice: 'Target macros were successfully updated.'
     else
       render :edit
     end
   end
-
   # DELETE /target_macros/1 or /target_macros/1.json
   def destroy
     @target_macro.destroy!
@@ -51,8 +46,15 @@ class TargetMacrosController < ApplicationController
 
   private
 
-    # Only allow a list of trusted parameters through.
-    def target_macro_params
-      params.require(:target_macro).permit(:carb, :fat, :protein, :calories)
-    end
+  def set_or_create_target_macro
+    @target_macro = current_user.target_macro || current_user.create_target_macro(default_values)
+  end
+
+  def default_values
+    { fat: 0, protein: 0, carb: 0 }
+  end
+
+  def target_macro_params
+    params.require(:target_macro).permit(:carb, :fat, :protein, :calories)
+  end
 end
